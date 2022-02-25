57b92ad6bb77c5b9a93383a082f76c3c341e5dbb
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2022-02-25 17:55:11 +0300

    try start bot

--- src/index.ts
+++ src/index.ts
@@ -114,6 +114,17 @@ const bot = new Telegraf(process.env.BOT_TOKEN!);
 bot.command("oldschool", (ctx) => ctx.reply("Hello"));
 bot.command("modern", (ctx) => ctx.reply("Yo"));
 bot.command("hipster", Telegraf.reply("λ"));
+
+bot.start((ctx) => {
+  ctx.reply('Hello ' + ctx.from.first_name + '!');
+});
+
+bot.help((ctx) => {
+  ctx.reply('Send /start to receive a greeting');
+  ctx.reply('Send /keyboard to receive a message with a keyboard');
+  ctx.reply('Send /quit to stop the bot');
+});
+
 bot.launch();
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
