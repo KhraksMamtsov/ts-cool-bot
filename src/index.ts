26add46bf6ddb05c0106be28ecbdd75edc8d6d81
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2022-02-26 13:33:04 +0300

    try send code screenshoot 0

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import prism from "prismjs";
 import { format } from "prettier";
 import { promises as fs } from "fs";
 import { decompressFromEncodedURIComponent } from "lz-string";
-// import nodeHtmlToImage from "node-html-to-image";
+import nodeHtmlToImage from "node-html-to-image";
 import Handlebars from "handlebars";
 import { Telegraf } from "telegraf";
 import { getSubstring } from "./utils";
@@ -12,8 +12,6 @@ const CODEBLOCK_REGEX = /```(?:ts|typescript|js|javascript)?\n([\s\S]+)```/;
 export const PLAYGROUND_REGEX =
   /https?:\/\/(?:www\.)?(?:typescriptlang|staging-typescript)\.org\/(?:play|dev\/bug-workbench)(?:\/index\.html)?\/?(\??(?:\w+=[^\s#&]*)?(?:\&\w+=[^\s#&]*)*)#code\/([\w\-%+_]+={0,4})/;
 
-let resultHtml: string;
-
 function isNotUndefined<T>(x: T): x is Exclude<T, undefined> {
   return x !== undefined;
 }
@@ -21,105 +19,92 @@ function isNotUndefined<T>(x: T): x is Exclude<T, undefined> {
 function isNotNull<T>(x: T): x is Exclude<T, null> {
   return x !== null;
 }
-// try {
-//   // Make lines as short as reasonably possible, so they fit in the embed.
-//   // We pass prettier the full string, but only format part of it, so we can
-//   // calculate where the endChar is post-formatting.
-//   resultHtml = format(source, {
-//     parser: "typescript",
-//     printWidth: 55,
-//     tabWidth: 2,
-//     semi: false,
-//     bracketSpacing: false,
-//     arrowParens: "avoid",
-//     // rangeStart: startChar,
-//     // rangeEnd: endChar,
-//   });
-// } catch (e) {
-//   console.log("xxx", e);
-//   // Likely a syntax error
-//   resultHtml = source;
-// }
-// console.log(prism.plugins);
-// const htmlCode = prism.highlight(resultHtml, prism.languages["js"]!, "js");
 
-// async function start() {
-//   const pathsPromises = ["./src/index.hbs", "./src/styles.css"].map((x) => {
-//     return fs.readFile(x, "utf8");
-//   });
-//   const [htmlTemplate, styles] = await Promise.all(pathsPromises);
+async function start() {
+  const pathsPromises = ["./src/index.hbs", "./src/styles.css"].map((x) => {
+    return fs.readFile(x, "utf8");
+  });
+  const [htmlTemplate, styles] = await Promise.all(pathsPromises);
 
-//   const template = Handlebars.compile<{ style: string; code: string }>(
-//     htmlTemplate,
-//     {
-//       noEscape: true,
-//     }
-//   );
+  const template = Handlebars.compile<{ style: string; code: string }>(
+    htmlTemplate,
+    {
+      noEscape: true,
+    }
+  );
 
-//   const result = template({
-//     style: styles!,
-//     code: htmlCode,
-//   });
+  function getImageFromHtml(html: string) {
+    const htmlCode = prism.highlight(html, prism.languages["js"]!, "js");
+    const result = template({
+      style: styles!,
+      code: htmlCode,
+    });
 
-//   await fs.writeFile("./image.html", result);
-
-//   // await nodeHtmlToImage({
-//   //   output: "./image.png",
-//   //   quality: 100,
-//   //   selector: "code",
-//   //   html: result,
-//   //   puppeteerArgs: {
-//   //     defaultViewport: {
-//   //       height: 3000,
-//   //       width: 1000,
-//   //       deviceScaleFactor: 2,
-//   //     },
-//   //   },
-//   // }).then(() => console.log("The image was created successfully!"));
-// }
-
-// start();
-
-const bot = new Telegraf(process.env.BOT_TOKEN!);
-bot.on("text", async (ctx, next) => {
-  console.log("text:", ctx.message.entities);
-
-  const codeBlocks = ctx.message.entities
-    ?.filter((x) => x.type === "url")
-    .map((x) => getSubstring(ctx.message.text, x))
-    .map((x) => x.match(PLAYGROUND_REGEX))
-    .filter(isNotNull)
-    .map((x) => x[2])
-    .filter(isNotUndefined)
-    .map((x) => decompressFromEncodedURIComponent(x))
-    .filter(isNotNull)
-    .map((x) =>
-      format(x, {
-        parser: "typescript",
-        printWidth: 55,
-        tabWidth: 2,
-        semi: false,
-        bracketSpacing: false,
-        arrowParens: "avoid",
-      })
-    );
-
-  if (isNotUndefined(codeBlocks)) {
-    const answer = codeBlocks
-      .map((codeBlock) => "```\n" + codeBlock + "\n```")
-      .join("");
-
-    const asd = await ctx.replyWithHTML(answer, {
-      reply_to_message_id: ctx.message.message_id,
-      parse_mode: "Markdown",
-      disable_notification: true,
+    return nodeHtmlToImage({
+      quality: 100,
+      selector: "code",
+      html: result,
+      puppeteerArgs: {
+        defaultViewport: {
+          height: 3000,
+          width: 1000,
+          deviceScaleFactor: 2,
+        },
+      },
     });
   }
 
-  await next();
-});
+  const bot = new Telegraf(process.env.BOT_TOKEN!);
+  bot.on("text", async (ctx, next) => {
+    console.log("text:", ctx.message.entities);
 
-bot.launch();
+    const codeBlocks = ctx.message.entities
+      ?.filter((x) => x.type === "url")
+      .map((x) => getSubstring(ctx.message.text, x))
+      .map((x) => x.match(PLAYGROUND_REGEX))
+      .filter(isNotNull)
+      .map((x) => x[2])
+      .filter(isNotUndefined)
+      .map((x) => decompressFromEncodedURIComponent(x))
+      .filter(isNotNull)
+      .map((x) =>
+        format(x, {
+          parser: "typescript",
+          printWidth: 55,
+          tabWidth: 2,
+          semi: false,
+          bracketSpacing: false,
+          arrowParens: "avoid",
+        })
+      );
 
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+    if (isNotUndefined(codeBlocks)) {
+      // const answer = codeBlocks
+      //   .map((codeBlock) => "```\n" + codeBlock + "\n```")
+      //   .join("");
+
+      const answer = codeBlocks
+        .map((codeBlock) => getImageFromHtml(codeBlock) as Promise<Buffer>)
+        .map(async (x) => {
+          const asd = await x;
+          ctx.replyWithPhoto(
+            {
+              source: asd,
+            },
+            {
+              reply_to_message_id: ctx.message.message_id,
+            }
+          );
+        });
+    }
+
+    await next();
+  });
+
+  bot.launch();
+
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
+}
+
+start();
